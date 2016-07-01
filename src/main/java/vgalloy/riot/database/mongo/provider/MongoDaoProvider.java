d226b6dd5d875a49ab7f49f552c7eb744dca754e
Vincent Galloy <vgalloy@excilys.com>
2016-07-01 16:03:50 +0200

    [ NEW ] : Add RecentGameDao in mongo provider

--- src/main/java/vgalloy/riot/database/mongo/provider/MongoDaoProvider.java
+++ src/main/java/vgalloy/riot/database/mongo/provider/MongoDaoProvider.java
@@ -2,10 +2,12 @@ package vgalloy.riot.database.mongo.provider;
 
 import vgalloy.riot.database.mongo.dao.MatchDetailDao;
 import vgalloy.riot.database.mongo.dao.RankedStatsDao;
+import vgalloy.riot.database.mongo.dao.RecentGamesDao;
 import vgalloy.riot.database.mongo.dao.SummonerDao;
 import vgalloy.riot.database.mongo.dao.factory.DaoFactory;
 import vgalloy.riot.database.mongo.dao.impl.MatchDetailDaoImpl;
 import vgalloy.riot.database.mongo.dao.impl.RankedStatsDaoImpl;
+import vgalloy.riot.database.mongo.dao.impl.RecentGamesDaoImpl;
 import vgalloy.riot.database.mongo.dao.impl.SummonerDaoImpl;
 import vgalloy.riot.database.mongo.dao.query.QueryDao;
 import vgalloy.riot.database.mongo.dao.query.impl.QueryDaoImpl;
@@ -18,6 +20,16 @@ public enum MongoDaoProvider {
     INSTANCE;
 
     /**
+     * Get the matchDetailDao.
+     *
+     * @param databaseUrl the database url
+     * @return the matchDetailDao
+     */
+    public MatchDetailDao getMatchDetailDao(String databaseUrl) {
+        return DaoFactory.getDao(MatchDetailDaoImpl.class, databaseUrl, "riot2");
+    }
+
+    /**
      * Get the rankedStatsDao.
      *
      * @param databaseUrl the database url
@@ -28,13 +40,13 @@ public enum MongoDaoProvider {
     }
 
     /**
-     * Get the matchDetailDao.
+     * Get the RecentGamesDao.
      *
      * @param databaseUrl the database url
-     * @return the matchDetailDao
+     * @return the RecentGamesDao
      */
-    public MatchDetailDao getMatchDetailDao(String databaseUrl) {
-        return DaoFactory.getDao(MatchDetailDaoImpl.class, databaseUrl, "riot2");
+    public RecentGamesDao getRecentGamesDao(String databaseUrl) {
+        return DaoFactory.getDao(RecentGamesDaoImpl.class, databaseUrl, "riot2");
     }
 
     /**
@@ -46,7 +58,6 @@ public enum MongoDaoProvider {
     public SummonerDao getSummonerDao(String databaseUrl) {
         return DaoFactory.getDao(SummonerDaoImpl.class, databaseUrl, "riot2");
     }
-
 
     /**
      * Get the queryDao.
